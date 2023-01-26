43ba116598474578bb7908ff4a379278ef483dc9
Lupin Cai <lupin.cai@emory.edu>
2023-01-25 22:04:26 -0500

    Initial commit

--- src/test/java/edu/emory/cs/algebraic/LongIntegerQuizTest.java
+++ src/test/java/edu/emory/cs/algebraic/LongIntegerQuizTest.java
@@ -7,71 +7,71 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LongIntegerQuizTest {
     @Test
     public void test0() {
-        LongInteger a = new LongIntegerQuiz("123");
+        LongInteger a = new LongIntegerQuiz("875");
         a.add(new LongIntegerQuiz("0"));
-        assertEquals("123", a.toString());
+        assertEquals("875", a.toString());
     }
 
     @Test
     public void test2() {
-        LongInteger a = new LongIntegerQuiz("123");
+        LongInteger a = new LongIntegerQuiz("789");
         a.add(new LongIntegerQuiz("-0"));
-        assertEquals("123", a.toString());
+        assertEquals("789", a.toString());
     }
 
     @Test
     public void test3() {
-        LongInteger a = new LongIntegerQuiz("123");
-        a.add(new LongIntegerQuiz("-123"));
+        LongInteger a = new LongIntegerQuiz("875");
+        a.add(new LongIntegerQuiz("-875"));
         assertEquals("0", a.toString());
     }
 
     @Test
     public void test4() {
-        LongInteger a = new LongIntegerQuiz("-123");
-        a.add(new LongIntegerQuiz("123"));
+        LongInteger a = new LongIntegerQuiz("-875");
+        a.add(new LongIntegerQuiz("875"));
         assertEquals("0", a.toString());
     }
 
     @Test
     public void test5() {
-        LongInteger a = new LongIntegerQuiz("123");
-        a.add(new LongIntegerQuiz("-122"));
+        LongInteger a = new LongIntegerQuiz("714");
+        a.add(new LongIntegerQuiz("-713"));
         assertEquals("1", a.toString());
     }
 
     @Test
     public void test6() {
-        LongInteger a = new LongIntegerQuiz("-123");
-        a.add(new LongIntegerQuiz("122"));
+        LongInteger a = new LongIntegerQuiz("-714");
+        a.add(new LongIntegerQuiz("713"));
         assertEquals("-1", a.toString());
     }
 
     @Test
     public void test7() {
-        LongInteger a = new LongIntegerQuiz("123");
-        a.add(new LongIntegerQuiz("-124"));
+        LongInteger a = new LongIntegerQuiz("713");
+        a.add(new LongIntegerQuiz("-714"));
         assertEquals("-1", a.toString());
     }
 
     @Test
     public void test8() {
-        LongInteger a = new LongIntegerQuiz("-123");
-        a.add(new LongIntegerQuiz("124"));
+        LongInteger a = new LongIntegerQuiz("-713");
+        a.add(new LongIntegerQuiz("714"));
         assertEquals("1", a.toString());
     }
 
     @Test
     public void test9() {
         LongInteger a = new LongIntegerQuiz("123");
-        a.add(new LongIntegerQuiz("-45678"));
-        assertEquals("-45555", a.toString());
+        a.add(new LongIntegerQuiz("-458793"));
+        assertEquals("-458670", a.toString());
     }
 
     @Test
     public void test10() {
-        LongInteger a = new LongIntegerQuiz("-12345");
-        a.add(new LongIntegerQuiz("678"));
-        assertEquals("-11667", a.toString());
+        LongInteger a = new LongIntegerQuiz("-87654");
+        a.add(new LongIntegerQuiz("123"));
+        assertEquals("-87531", a.toString());
     }
 }
