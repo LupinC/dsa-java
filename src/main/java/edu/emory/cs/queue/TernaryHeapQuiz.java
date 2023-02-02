c17c732395b5fbd13be162818d035eaad53ce96f
Lupin Cai <lupin.cai@emory.edu>
2023-02-02 09:30:02 -0500

    Initial commit

--- src/main/java/edu/emory/cs/queue/TernaryHeapQuiz.java
+++ src/main/java/edu/emory/cs/queue/TernaryHeapQuiz.java
@@ -30,18 +30,21 @@ public class TernaryHeapQuiz<T extends Comparable<T>> extends AbstractPriorityQu
     @Override
     public void add(T key) {
         keys.add(key);
-        swim(size()-1);
+        swim(size()-1);// the last index is at size()-1 since I start at 0
     }
 
     private void swim(int k) {
         for (; 0 < k && compare((k-1)/3, k) < 0; k = (k-1)/3)
             Collections.swap(keys, (k-1)/3, k);
+        //compare the index's value with its parent's,
+        // if the index's value has a higher priority
+        // than the parents, swap them
     }
 
     @Override
     public T remove() {
         if (isEmpty()) return null;
-        Collections.swap(keys, 0, size()-1);
+        Collections.swap(keys, 0, size()-1);//swap the max priority with the last one
         T max = keys.remove(size()-1);
         sink();
         return max;
@@ -54,8 +57,8 @@ public class TernaryHeapQuiz<T extends Comparable<T>> extends AbstractPriorityQu
 
     private void sink(){
         for(int k = 0, i = 1; i<size(); k = i, i = 3*i+1){
-
-
+            //if the number of children of this parent is 3
+            //see if any of the second two is larger than the first one
             if(i+2< size())
             {
                 if(compare(i,i+1)<0||compare(i,i+2)<0)
@@ -63,11 +66,7 @@ public class TernaryHeapQuiz<T extends Comparable<T>> extends AbstractPriorityQu
                     i++;
                 }
             }
-
             if (i+1 < size() && compare(i, i + 1) < 0) i++;
-
-
-
 
             if (compare(k,i)>=0) break;
             Collections.swap(keys,k,i);
