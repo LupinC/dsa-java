5be15bce06c6a714b41323b9588327dbeaaf7764
Lupin Cai <lupin.cai@emory.edu>
2023-04-17 15:10:22 -0400

    Initial commit

--- src/main/java/edu/emory/cs/trie/autocomplete/AutocompleteHW.java
+++ src/main/java/edu/emory/cs/trie/autocomplete/AutocompleteHW.java
@@ -113,7 +113,7 @@ public class AutocompleteHW extends Autocomplete<List<String>> {
         while (!queue.isEmpty()) {
             TrieNode<List<String>> currNode = queue.poll();
 
-            if(output.size()==getMax()+count+2)
+            if(output.size()==getMax()+count+4)
             {
                 break;
             }
