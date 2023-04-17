e9360139a1f6665ce7ea509ad736a26bc06e838a
Lupin Cai <lupin.cai@emory.edu>
2023-04-17 12:21:27 -0400

    Initial commit

---
+++ src/main/java/edu/emory/cs/graph/flow/NetworkFlowQuiz.java
@@ -0,0 +1,22 @@
+package edu.emory.cs.graph.flow;
+
+import edu.emory.cs.graph.Graph;
+import edu.emory.cs.graph.Subgraph;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/** @author Jinho D. Choi */
+public class NetworkFlowQuiz {
+    /**
+     * Using depth-first traverse.
+     * @param graph  a directed graph.
+     * @param source the source vertex.
+     * @param target the target vertex.
+     * @return a set of all augmenting paths between the specific source and target vertices in the graph.
+     */
+    public Set<Subgraph> getAugmentingPaths(Graph graph, int source, int target) {
+        // TODO: to be updated
+        return null;
+    }
+}
